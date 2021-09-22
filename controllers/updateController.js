7a83ce328f5cf8e9be02bf59b942c38a41f3a786
JuberBadi <juberbadi052@gmail.com>
2021-09-22 18:14:01 +0530

    Validation in CRUD  Operation using Express-Validator

---
+++ controllers/updateController.js
@@ -0,0 +1,37 @@
+const db_connection = require('../models/dbConnection').promise();
+
+exports.updateUser = async (req, res, next) => {
+    try {
+  
+      const [row] = await db_connection.execute(
+          "SELECT * FROM `users` WHERE `id`=?",
+          [req.params.id]
+      );
+  
+      if (row.length === 0) {
+        return res.status(404).json({
+          message: "Invalid User ID",
+        });
+      }
+  
+      if (req.body.name) row[0].name = req.body.name;
+  
+      if (req.body.email) row[0].email = req.body.email;
+  
+      const [update] = await db_connection.execute(
+        "UPDATE `users` SET `name`=?, `email`=? WHERE `id`=?",
+        [row[0].name, row[0].email, req.params.id]
+      );
+  
+      if (update.affectedRows === 1) {
+        return res.json({
+          message: "The User has been successfully updated.",
+        });
+      }
+  
+    } catch (err) {
+      next(err);
+    }
+  
+  };
+  
